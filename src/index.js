898d3bc31d05e1c51277621970f96f91919aceba
redxtreme <zahopper@mtu.edu>
2017-12-15 10:58:27 -0600

    Bundling with Webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+
+// insert a breakpoint for web debugger
+//debugger;
+
+// ` means there's variables in the string
+console.log(`I would pay ${courseValue} for this awesome course!`);
