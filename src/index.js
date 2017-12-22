fb24b0ae54612ec43d00b5656ec6c9a503a52b80
redxtreme <zahopper@mtu.edu>
2017-12-22 10:51:05 -0600

    Fetch working
    Uses polyfill fetch because some browsers don’t support it fully

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
+//import numeral from 'numeral';
+//const courseValue = numeral(1000).format('$0,0.00');
+//// insert a breakpoint for web debugger
+////debugger;
+//// ` means there's variables in the string
+//console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
 
-const courseValue = numeral(1000).format('$0,0.00');
+// Populate table of users via API promise call.
+getUsers().then(result => {
+	let usersBody = "";
 
-// insert a breakpoint for web debugger
-//debugger;
+	result.forEach(user => {
+		usersBody += `<tr>
+		<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+		<td>${user.id}</td>
+		<td>${user.firstName}</td>
+		<td>${user.lastname}</td>
+		<td>${user.email}</td>
+		</tr>`
+	});
 
-// ` means there's variables in the string
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+	global.document.getElementById('users').innerHTML = usersBody;
+});
