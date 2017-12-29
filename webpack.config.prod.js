ac39ca725854a13e3c77683c81cfa47a9e09b435
redxtreme <zahopper@mtu.edu>
2017-12-29 09:56:23 -0600

    Supposedly Bundle Split
    It doesn’t look like main.js got any smaller and vendor.js isn’t very
    big

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,52 +4,63 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  debug: true,
+	debug: true,
 
-		// Slower than inline-source-map,
-		// but still allows us to see full code even when minified/transpiled/bundled
-  devtool: 'source-map',
-  noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
-  target: 'web',
-  output: {
+	// Slower than inline-source-map,
+	// but still allows us to see full code even when minified/transpiled/bundled
+	devtool: 'source-map',
+	noInfo: false,
+	entry: {
+		// Vendor.js bundle entry point where 3rd party tools are loaded
+		vendor: path.resolve(__dirname, 'src/vendor'),
+		main: path.resolve(__dirname, 'src/index')
+	},
+	target: 'web',
+	output: {
+		// Dist stands for distribution
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
 
-				// Dist stands for distribution
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-			// Dynamically generate HTML file that includes reference to bundled JS.
-			new HtmlWebpackPlugin({
-				template: 'src/index.html',
-				minify: {
-					removeComments: true,
-					collapseWhitespace: true,
-					removeRedundantAttributes: true,
-					useShortDoctype: true,
-					removeEmptyAttributes: true,
-					removeStyleLinkTypeAttributes: true,
-					keepClosingSlash: true,
-					minifyJS: true,
-					minifyCSS: true,
-					minifyURLs: true
-				},
-				inject: true
-			}),
+		// Generate entrpoint named files
+		filename: '[name].js'
+	},
+	plugins: [
+		// Use CommonsChunkPlugin to create a separate bundle
+		// of vendor libraries so that they're cached separately.
+		// Leaves any imports in these chunks out of the separate bundle (main.js)
+		new webpack.optimize.CommonsChunkPlugin({
+			// Use the same name as the entry point key
+			name: 'vendor'
+		}),
 
-			// Eliminate duplicate packages when generating bundle
-			new webpack.optimize.DedupePlugin(),
+		// Dynamically generate HTML file that includes reference to bundled JS.
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			},
+			inject: true
+		}),
 
-			// Minify JS
-			new webpack.optimize.UglifyJsPlugin()
-		],
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
-    ]
-  }
+		// Eliminate duplicate packages when generating bundle
+		new webpack.optimize.DedupePlugin(),
+
+		// Minify JS
+		new webpack.optimize.UglifyJsPlugin()
+	],
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+			{test: /\.css$/, loaders: ['style','css']}
+		]
+	}
 }
